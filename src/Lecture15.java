6a680c0e53edf2b5b1f88d3981c53d2badec2167
Your Name <you@example.com>
2024-02-28 06:17:19 -0800

    Initial Commit

---
+++ src/Lecture15.java
@@ -0,0 +1,39 @@
+public class Lecture15 {
+
+  public static void main(String[] args) {
+    System.out.println();
+    System.out.println("Hello, world!");
+    // SELF-CHECK PROBLEMS:
+    // Note: you will need to put some of your answers inside a comments
+
+    // Put your answer for #18 here:
+
+    // Put your answer for #19 here:
+
+    // Put your answer for #20 here:
+
+    // Put your answer for #21 here:
+
+    // Put your answer for #23 here:
+
+    // Put your answer for #24 here:
+
+    // Put your answer for #25 here:
+
+    // Put your answer for #26 here:
+
+    // Put your answer for #27 here:
+
+    // Put your answer for #28 here:
+
+    // EXERCISES:
+
+    // Put your answer for #11 here:
+    // You will need to add the method in BankAccount.java,
+    // and then create objects here to use (test) them
+
+    // Put your answer for #12 here:
+
+    // Put your answer for #13 here:
+  }
+}
